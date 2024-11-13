45ad5f878cde8e96b01fcec39da80c0cac92ac17
Julliet Kgomotso Rakuba <jullietrakuba@gmail.com>
2024-11-13 08:26:56 +0200

    Revert "Dev"

--- server.js
+++ server.js
@@ -1,43 +1,23 @@
 // Imported the express module
 const express = require('express');
-const bodyParser = require('body-parser');
 
 // Initialized the Express app
 const app = express();
-const port = 3000;
 
 // Middleware to parse JSON data in the body
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-
-//in memmory array to hold todo items
-let todos =[];
+app.use(express.json());
 
 // Default route: Home page
-app.get('/todos', (req, res) => {
-    res.json(todos);
+app.get('/', (req, res) => {
+    res.send('Welcome to the default server!');
 });
 
-
-app.post('/todos', (req, res) => {
-    const {id} = req.params;
-    const { compeleted } = req.body;
-    const todo = todos.find(todo => todo.id == id);
-    if (todo) {
-
-        todo.compeleted = completed;
-        res.json(todo);
-    }else {
-
-        res.status(404).json({ error: 'Todo not found'});
-    }
-});
-
-app.delete('/todos/:id', (req, res) => {
-const { id } = req.params;
-todos = todos.filter(todo => todo.id != id);
-res.json({ message: 'Todo deleted successfully'});
+// API route: Get data from the server
+app.get('/api/data', (req, res) => {
+    res.json({
+        message: 'This is some data from the server.',
+        data: [1, 2, 3, 4, 5]
+    });
 });
 
 // Port set to 3000
